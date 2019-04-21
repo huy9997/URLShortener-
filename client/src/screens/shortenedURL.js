adcdec684620e5774244c4b24b8a29915e7284ee
Huy Nguyen <huy9997@gmail.com>
2019-04-20 20:06:28 -0700

    working on fetch request frontend to backend

--- client/src/screens/shortenedURL.js
+++ client/src/screens/shortenedURL.js
@@ -1,27 +1,47 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import axios from 'axios'
 
-const container = styled.div`
+const Contain = styled.div`
   display:flex;
   flex-direction:column;
   align-items:center;
 `;
-
 class shortenedURL extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      inputURL: '',
+      inputURL: ''
     }
+  }
+  
+  handleChange = e => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+   }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+      axios.post('/api/shortenedURL', {
+        urlData: this.state.inputURL
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
   render(){
     return(
-      <container>
-        <Input></Input>
-        <Button>Submit</Button>
-      </container>
+      <Contain>
+        <Input onChange={this.handleChange} placeholder="Enter URL"></Input>
+        <Button onClick={this.handleSubmit}>Submit</Button>
+      </Contain>
     );
   }
 }
